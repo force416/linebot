146bb00b90990f4644b3750c48f8c6874fb88727
eric.shih <force416@gmail.com>
2016-07-22 10:04:11 +0800

    新增取得傳訊者資料

--- src/main/java/eric/linebot/manager/impl/LineClientManagerImpl.java
+++ src/main/java/eric/linebot/manager/impl/LineClientManagerImpl.java
@@ -1,5 +1,6 @@
 package eric.linebot.manager.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -15,34 +16,49 @@ import com.linecorp.bot.model.callback.Event;
 import com.linecorp.bot.model.content.AbstractContent;
 import com.linecorp.bot.model.content.Content;
 import com.linecorp.bot.model.content.TextContent;
+import com.linecorp.bot.model.profile.UserProfileResponse;
 
 import eric.linebot.manager.LineClientManger;
 
 @Service("lineClientManager")
 public class LineClientManagerImpl implements LineClientManger {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Resource
 	private LineBotClient lineBotClient;
-	
+
 	private Gson gson = new Gson();
-	
+
 	@Override
 	public boolean callback(List<Event> events) throws LineBotAPIException {
 		for (Event event : events) {
 			Content content = event.getContent();
+			String userMid = null;
+			//訊息
 			if (content instanceof TextContent) {
 				TextContent text = (TextContent) content;
 				lineBotClient.sendText(text.getFrom(), text.getText());
+				userMid = text.getFrom();
 			} else {
+			//其它種類訊息
 				AbstractContent ct = (AbstractContent) content;
 				logger.debug(gson.toJson(ct));
 				lineBotClient.sendText(ct.getFrom(), "pi~ka~chu~~~");
-				lineBotClient.sendImage(ct.getFrom(), "http://i.imgur.com/n5IwKuu.png", "http://i.imgur.com/n5IwKuu.png");
+				lineBotClient.sendImage(ct.getFrom(),
+						"http://i.imgur.com/n5IwKuu.png",
+						"http://i.imgur.com/n5IwKuu.png");
+				userMid = ct.getFrom();
 			}
+			
+			//取得傳訊息user資訊
+			List<String> userProfileList = new ArrayList<String>();
+			userProfileList.add(userMid);
+			UserProfileResponse rp = lineBotClient.getUserProfile(userProfileList);
+			logger.debug(gson.toJson(rp.getContacts().get(0)));
 		}
+		
 		return false;
 	}
-	
+
 }
