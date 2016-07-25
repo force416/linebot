fe3b1c26b46d06f0cd5764856708fc36350b454a
eric.shih <force416@gmail.com>
2016-07-25 10:51:21 +0800

    add exception handle

--- src/main/java/eric/linebot/service/LineService.java
+++ src/main/java/eric/linebot/service/LineService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.linecorp.bot.client.exception.LineBotAPIException;
+import com.linecorp.bot.client.exception.LineBotServerErrorStatusException;
 import com.linecorp.bot.model.callback.Event;
 import com.linecorp.bot.spring.boot.annotation.LineBotMessages;
 
@@ -16,6 +19,7 @@ import eric.linebot.manager.LineClientManger;
 @RestController()
 @RequestMapping("/line-service")
 public class LineService {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Resource
 	private LineClientManger lineClientManger;
@@ -27,7 +31,11 @@ public class LineService {
 	 */
 	@RequestMapping("/callback")
 	public void callback(@LineBotMessages List<Event> events) throws LineBotAPIException {
-		lineClientManger.callback(events);
+		try {
+			lineClientManger.callback(events);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
 	}
 	
 }
