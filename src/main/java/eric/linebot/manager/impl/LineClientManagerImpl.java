62f917c0d65bf91af19324294081ad5a9fdc0478
eric.shih <force416@gmail.com>
2016-08-06 13:02:57 +0800

    edit format

--- src/main/java/eric/linebot/manager/impl/LineClientManagerImpl.java
+++ src/main/java/eric/linebot/manager/impl/LineClientManagerImpl.java
@@ -77,8 +77,7 @@ public class LineClientManagerImpl implements LineClientManger {
 			// 取得傳訊息user資訊
 			List<String> userProfileList = new ArrayList<String>();
 			userProfileList.add(userMid);
-			UserProfileResponse rp = lineBotClient
-					.getUserProfile(userProfileList);
+			UserProfileResponse rp = lineBotClient.getUserProfile(userProfileList);
 			logger.info(gson.toJson(rp.getContacts().get(0)));
 		}
 
@@ -98,12 +97,9 @@ public class LineClientManagerImpl implements LineClientManger {
 	 *            發送對象mid
 	 * @throws Exception
 	 */
-	private void queryImgurImageAndSendMessage(String keyword, String mid)
-			throws Exception {
+	private void queryImgurImageAndSendMessage(String keyword, String mid) throws Exception {
 		// 關鍵字查詢imgur圖片
-		List<ImgurImageModel> imgurImageModelList = imgurApiService
-				.gallerySearch(new ImgurSearchImageModel(URLEncoder.encode(
-						keyword, "UTF-8"), "top", "", ""));
+		List<ImgurImageModel> imgurImageModelList = imgurApiService.gallerySearch(new ImgurSearchImageModel(URLEncoder.encode(keyword, "UTF-8"), "top", "", ""));
 		if (imgurImageModelList == null || imgurImageModelList.isEmpty()) {
 			this.sendDefaultMessage(mid);
 		}
@@ -113,8 +109,7 @@ public class LineClientManagerImpl implements LineClientManger {
 				break;
 			}
 			// 圖片的link才發送訊息
-			if (model.getLink().indexOf(".jpg") != -1
-					|| model.getLink().indexOf(".png") != -1) {
+			if (model.getLink().indexOf(".jpg") != -1 || model.getLink().indexOf(".png") != -1) {
 				cnt++;
 				lineBotClient.sendImage(mid, model.getLink(), model.getLink());
 			}
@@ -130,18 +125,14 @@ public class LineClientManagerImpl implements LineClientManger {
 	 *            發送對象mid
 	 * @throws Exception
 	 */
-	private void queryBingImageAndSendMessage(String keyword, String mid)
-			throws Exception {
+	private void queryBingImageAndSendMessage(String keyword, String mid) throws Exception {
 		// 關鍵字查詢圖片
-		List<BingImageModel> bingImageModelList = bingSearchApiService
-				.imageSearch(new BingImageSearchModel(URLEncoder.encode(
-						keyword, "UTF-8"), 10));
+		List<BingImageModel> bingImageModelList = bingSearchApiService.imageSearch(new BingImageSearchModel(URLEncoder.encode(keyword, "UTF-8"), 10));
 		if (bingImageModelList == null || bingImageModelList.isEmpty()) {
 			this.sendDefaultMessage(mid);
 		}
 		for (BingImageModel model : bingImageModelList) {
-			lineBotClient.sendImage(mid, model.getContentUrl(),
-					model.getContentUrl());
+			lineBotClient.sendImage(mid, model.getContentUrl(), model.getContentUrl());
 		}
 	}
 
@@ -154,24 +145,18 @@ public class LineClientManagerImpl implements LineClientManger {
 	private void queryAVAndSendMessage(String keyword, String mid) throws Exception {
 		List<AVModel> modelList = aVParserManager.search(keyword);
 		if (modelList != null && !modelList.isEmpty()) {
-			//只有一筆資料
+			// 只有一筆資料
 			if (modelList.size() == 1) {
 				AVModel model = modelList.get(0);
-				lineBotClient.sendText(mid,
-								"番號 : " + model.getId() + "\n" + 
-								"片名 : " + model.getName() + "\n" + 
-								"發行日期 : "+ model.getDate());
+				lineBotClient.sendText(mid, "番號 : " + model.getId() + "\n" + "片名 : " + model.getName() + "\n" + "發行日期 : " + model.getDate());
 				lineBotClient.sendImage(mid, model.getCoverUrl(), model.getCoverUrl());
 				for (String filmStill : model.getFilmStillList()) {
 					lineBotClient.sendImage(mid, filmStill, filmStill);
 				}
 			} else {
-				//多筆資料
+				// 多筆資料
 				for (AVModel model : modelList) {
-					lineBotClient.sendText(mid,
-									"番號 : " + model.getId() + "\n" + 
-									"片名 : " + model.getName() + "\n" + 
-									"發行日期 : "+ model.getDate());
+					lineBotClient.sendText(mid, "番號 : " + model.getId() + "\n" + "片名 : " + model.getName() + "\n" + "發行日期 : " + model.getDate());
 					lineBotClient.sendImage(mid, model.getCoverUrl(), model.getCoverUrl());
 				}
 			}
